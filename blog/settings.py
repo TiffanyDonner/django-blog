b56413a6d0c69c877824d3125e7bd9c1f17bee92
Tiffany Donner <tiffany.donner@stsupport.se>
2020-05-21 11:46:46 +0000

    Added Allowed host of os.environ.get(C9_HOSTNAME) to settings.py file

--- blog/settings.py
+++ blog/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['blog-test-app165.herokuapp.com']
+ALLOWED_HOSTS = [os.environ.get("C9_HOSTNAME"), 'blog-test-app165.herokuapp.com']
 
 
 # Application definition
